7298e5fe1b0884b3d8d00b08a10c82814e77dc9b
les-knick <lea.stocker@knickdesign.de>
2021-09-29 17:18:08 +0200

    let the fun begin.

--- functions.php
+++ functions.php
@@ -1,9 +1,14 @@
 <?php
-/* enqueue script for parent theme stylesheeet */        
-function childtheme_parent_styles() {
- 
- // enqueue style
- wp_enqueue_style( 'parent', get_template_directory_uri().'/style.css' );                       
-}
-add_action( 'wp_enqueue_scripts', 'childtheme_parent_styles');
-?>
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    $parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(),  // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
+}?>
